023b26995ef655d155b1ed7b54889d69bb5c2820
Sanghee Kim <sangheestyle@gmail.com>
2019-04-18 20:40:45 +0300

    simple hello world web server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # naive_rust_web_server
 A naive rust web server
+
+## How to run
+
+```
+cargo build
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![feature(async_await)]
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = tide::App::new(());
+    app.at("/").get(async move |_| "Hello, world!");
+    app.serve("127.0.0.1:8000").unwrap();
 }
