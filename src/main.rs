f42167dcdd33a3cd92f55c258319e7426a83e25d
Sanghee Kim <sangheestyle@gmail.com>
2019-04-21 13:07:13 +0300

    routing and parameters

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-#![feature(async_await)]
+#![feature(async_await, futures_api)]
+
+use tide::error::ResultExt;
+
+async fn hello(cx: tide::Context<()>) -> tide::EndpointResult<String> {
+    let user: String = cx.param("user").client_err()?;
+    Ok(format!("Hello, {}!", user))
+}
+
+async fn goodbye(cx: tide::Context<()>) -> tide::EndpointResult<String> {
+    let user: String = cx.param("user").client_err()?;
+    Ok(format!("Goodbye, {}.", user))
+}
 
 fn main() {
     let mut app = tide::App::new(());
-    app.at("/").get(async move |_| "Hello, world!");
+
+    app.at("/hello/:user").get(hello);
+    app.at("/goodbye/:user").get(goodbye);
+    app.at("/").get(async move |_| {
+        "Use /hello/{your name} or /goodbye/{your name}"
+    });
+
     app.serve("127.0.0.1:8000").unwrap();
 }
